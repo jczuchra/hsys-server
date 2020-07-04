2f9fbc73efd262aa6974a0462a4231cc7a0b1c71
jczuchra <czuchra.jakub@gmail.com>
2020-07-04 15:13:13 +0200

    Add process.env.PORT

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ app.use((req, _, next) => {
 server.applyMiddleware({ app, cors: false });
 
 models.sequelize.sync().then(() => {
-  app.listen({ port: 4000 }, () => {
+  app.listen({ port: process.env.PORT || 4000 }, () => {
     console.log(
       `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
     );
